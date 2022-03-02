8541a6a3d638d724c9ff04c18bef37dc125db7fe
Reynaldi1912 <nexrey19@gmail.com>
2022-03-02 09:26:52 +0700

    pertemuann3

---
+++ Praktikum-Pertemuan3/react/react-app/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HelloComponent from './component/HelloComponent'; 
+import List from './List';
+import Test from './Test';
+import Login from './component/loginComponent'; 
+
+
+
+// function HelloComponent() {
+//   return <p>Hello World</p>
+// }
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+
+
+// class Statefullcomponent extends React.components{
+//   render(){
+//     return <p>StateFullComponent</p>
+//   }
+// }
+
+// ReactDOM.render(<Statefullcomponent/>,document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+ServiceWorker.unregister();
