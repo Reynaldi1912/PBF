25a7b5bd7ae62db4119e96c849a06ae3f08ae41e
Reynaldi1912 <nexrey19@gmail.com>
2022-05-31 19:23:19 +0700

    Praktikum 12

---
+++ Praktikum-Pertemuan12/my-app/src/firebase/config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-3V9tZNmF0PuK5wAgUIO2Ey1dQGU7O28",
+  authDomain: "modul11-165b2.firebaseapp.com",
+  projectId: "modul11-165b2",
+  storageBucket: "modul11-165b2.appspot.com",
+  messagingSenderId: "165163241506",
+  appId: "1:165163241506:web:97c73dd2e06d9c68c83a7b"
+};
+
+export default firebaseConfig;
