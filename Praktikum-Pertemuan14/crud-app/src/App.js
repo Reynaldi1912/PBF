321b0124da8d087ad176fae56679db2fd954840d
Reynaldi1912 <nexrey19@gmail.com>
2022-06-08 07:33:55 +0700

    praktikum 14

---
+++ Praktikum-Pertemuan14/crud-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './container/students';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Student/>
+    </div>
+  );
+}
+
+export default App;
